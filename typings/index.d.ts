358872a6acb4e0036db632dc3bc068f7590d180f
Dima Zherebko <kraken@live.ru>
2016-09-08 12:13:33 +0300

    add typigs

---
+++ typings/index.d.ts
@@ -0,0 +1,9 @@
+/// <reference path="es6-shim/es6-shim.d.ts" />
+/// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="modules/chai/index.d.ts" />
+/// <reference path="modules/requirejs/index.d.ts" />
+
+
+declare module 'url' {
+    export interface Url {}
+}
